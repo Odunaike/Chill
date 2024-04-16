f72fda3e9c39e8e0b7e561ec6c0a0023175abb0f
davidodunaike <odunaikedavid21@gmail.com>
2024-04-16 04:01:35 +0100

    Added carousel in these commit

--- app/src/main/java/com/example/chill/presentation/Home/MoviesScreen.kt
+++ app/src/main/java/com/example/chill/presentation/Home/MoviesScreen.kt
@@ -1,32 +1,48 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.example.chill.presentation.Home
 
-import android.app.Application
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import coil.size.Size
 import com.example.chill.R
-import com.example.chill.data.remote.MovieApi
-import com.example.chill.domain.model.MovieItem
 import com.example.chill.domain.model.Result
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 
 @Composable
@@ -56,19 +72,79 @@ fun MovieItemCard(
 fun MovieGrid(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    var myList: List<Result> = viewModel.state.movieList
-
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-    ) {
-        items(myList) {
-            it.poster_path?.let { img_path ->
-                MovieItemCard(
-                    modifier = Modifier.padding(8.dp),
-                    imgPath = img_path
-                )
+    Column {
+        MovieCarousel()
+        var myList: List<Result> = viewModel.state.movieList
+        Text(
+            text = "Discover",
+            fontSize = 30.sp
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+        ) {
+            items(myList) {
+                it.poster_path?.let { img_path ->
+                    MovieItemCard(
+                        modifier = Modifier.padding(8.dp),
+                        imgPath = img_path
+                    )
+                }
             }
-            
+        }
+    }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun MovieCarousel(){
+    val imageList = listOf(
+        R.drawable.movie1,
+        R.drawable.movie2,
+        R.drawable.movies3
+    )
+    val pagerState = rememberPagerState(pageCount = {3})
+    HorizontalPager(
+        state = pagerState,
+        pageSize = PageSize.Fixed(360.dp),
+        pageSpacing = 20.dp,
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(RoundedCornerShape(20.dp)),
+            painter = painterResource(id = imageList[it]),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds
+        )
+    }
+    //row with circle indicator for the current page
+    Row(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        repeat(pagerState.pageCount){
+            val color = if (pagerState.currentPage == it) Color.Magenta else Color.LightGray
+            Box(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(10.dp)
+            )
+        }
+    }
+    LaunchedEffect(key1 = pagerState.settledPage,){
+        delay(3000)
+        with(pagerState){
+            val target = if (settledPage < pageCount-1) settledPage+1 else 0
+            animateScrollToPage(
+                page = target,
+                animationSpec = tween(durationMillis = 1000)
+            )
         }
     }
 }
