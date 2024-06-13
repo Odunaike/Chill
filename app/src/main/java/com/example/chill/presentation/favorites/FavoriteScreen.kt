636bc1cd38f11a765f4307806de7b603a7fcfdc6
davidodunaike <odunaikedavid21@gmail.com>
2024-06-13 23:21:12 +0100

    Implemented item removal from favorites
    Tested this approach using touch to remove an item in this commit

--- app/src/main/java/com/example/chill/presentation/favorites/FavoriteScreen.kt
+++ app/src/main/java/com/example/chill/presentation/favorites/FavoriteScreen.kt
@@ -1,7 +1,9 @@
 package com.example.chill.presentation.favorites
 
-import androidx.compose.foundation.Image
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,6 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -23,25 +26,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.example.chill.R
 import com.example.chill.domain.model.FavMovieModel
 import com.example.chill.presentation.Authentication.AuthenticationViewModel
 
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun FavoritesScreen(
     authViewModel: AuthenticationViewModel
 ){
-    val userFavListRemote by remember {
+    val userFavListFromViewModel by remember {
         mutableStateOf(authViewModel.retrieveUserFavList())
     }
+
+    // using mutableState in favorite screen as in the last commit will not
+    // observe modifications to the list like adding or deleting. So my favorite screen ui was not responsive(recomposing) even though the delete worked
+    // Using mutableStateListOf I am guaranteed the ui will recompose
+
+    val mutableFavList = remember{
+        mutableStateListOf<FavMovieModel>()
+    }
+
+     mutableFavList.addAll(userFavListFromViewModel)
 
     Column {
         Spacer(modifier = Modifier.height(50.dp))
@@ -52,29 +63,39 @@ fun FavoritesScreen(
             modifier = Modifier.padding(start = 10.dp)
         )
         Spacer(modifier = Modifier.height(10.dp))
-        FavList(userFavListRemote.toList())
-    }
-}
-
-@Composable
-fun FavList(favList: List<FavMovieModel>){
-    LazyColumn(){
-        items(
-            items = favList
-        ){
-            FavItem(it)
+        FavList(mutableFavList) {item ->   //need a mutablelist to delete so I removed the toList() method
+            mutableFavList.remove(item)
+            authViewModel.saveUserFavList(
+                mutableFavList.toList()
+            )
         }
     }
 }
 
 @Composable
-fun FavItem(favItem: FavMovieModel){
+fun FavList(favList: MutableList<FavMovieModel>, todo: (FavMovieModel)-> Unit){
+    LazyColumn(){
+        items(
+            items = favList
+        ){
+            FavItem(favItem = it){item ->
+                todo(item)
+            }
+        }
+    }
+}
+
+@Composable
+fun FavItem(favItem: FavMovieModel, deleteFromFav: (item: FavMovieModel)-> Unit){
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 10.dp, vertical = 5.dp)
             .clip(shape = RoundedCornerShape(20.dp))
-            .background(color = MaterialTheme.colorScheme.primaryContainer),
+            .background(color = MaterialTheme.colorScheme.primaryContainer)
+            .clickable {
+                deleteFromFav(favItem)
+            },
         verticalAlignment = Alignment.CenterVertically
     ) {
         AsyncImage(
